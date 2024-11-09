21480340d7156a6cf90ebac3c05cfa834a5007d9
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-09 21:24:37 +0200

    change file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-/* eslint-disable react/jsx-key */
 import Header from './components/Header'
 import Section from './components/Section'
 import SectionButton from './components/SectionButton'
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
         <Section />
         <SectionButton />
       </main>
-    </div>
+    </>
   )
 }
 

08c4b9d73682a96e52d6c98d7b5427c3e92a2a7a
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-09 21:18:30 +0200

    new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,15 @@
 /* eslint-disable react/jsx-key */
 import Header from './components/Header'
-
-import Button from './components/Button/Button'
-import { useState } from 'react'
-import { differences } from '/src/data.js'
+import Section from './components/Section'
+import SectionButton from './components/SectionButton'
 
 function App() {
-  const [contentType, setContentType] = useState(null)
-
-  function handleClick(type) {
-    setContentType(type)
-  }
-
   return (
     <div>
       <Header />
       <main>
-        <section>
-          <h3>Отличия от других</h3>
-          <Button
-            active={contentType === 'way'}
-            onClick={() => handleClick('way')}
-            text="button 1"
-            title="Кнопка 1"
-          >
-            Подход
-          </Button>
-          <Button
-            active={contentType === 'easy'}
-            onClick={() => handleClick('easy')}
-            text=""
-            title="Кнопка 2"
-          >
-            Доступность
-          </Button>
-          <Button
-            active={contentType === 'program'}
-            onClick={() => handleClick('program')}
-            text="button 3"
-            title=""
-          >
-            Концентрация
-          </Button>
-
-          {contentType ? (
-            <p>{differences[contentType]}</p>
-          ) : (
-            <div>Нажми кнопку</div>
-          )}
-        </section>
+        <Section />
+        <SectionButton />
       </main>
     </div>
   )
---
+++ src/components/SectionButton.jsx
@@ -0,0 +1,46 @@
+import Button from './Button/Button'
+import { differences } from '../data.js'
+import { useState } from 'react'
+
+export default function SectionButton() {
+  const [contentType, setContentType] = useState(null)
+
+  function handleClick(type) {
+    setContentType(type)
+  }
+  return (
+    <section>
+      <h3>Отличия от других</h3>
+      <Button
+        active={contentType === 'way'}
+        onClick={() => handleClick('way')}
+        text="button 1"
+        title="Кнопка 1"
+      >
+        Подход
+      </Button>
+      <Button
+        active={contentType === 'easy'}
+        onClick={() => handleClick('easy')}
+        text=""
+        title="Кнопка 2"
+      >
+        Доступность
+      </Button>
+      <Button
+        active={contentType === 'program'}
+        onClick={() => handleClick('program')}
+        text="button 3"
+        title=""
+      >
+        Концентрация
+      </Button>
+
+      {contentType ? (
+        <p>{differences[contentType]}</p>
+      ) : (
+        <div>Нажми кнопку</div>
+      )}
+    </section>
+  )
+}

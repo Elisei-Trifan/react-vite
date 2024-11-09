422672ded3452d7f66780219a51abc932716c49b
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-09 20:43:41 +0200

    dinamic add class

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { ways, differences } from '/src/data.js'
 function App() {
   const [contentType, setContentType] = useState(null)
 
-  function handleClick(type = ' ') {
+  function handleClick(type) {
     setContentType(type)
   }
 
@@ -25,23 +25,30 @@ function App() {
         <section>
           <h3>Отличия от других</h3>
           <Button
-            active="active"
+            active={contentType === 'way'}
             onClick={() => handleClick('way')}
             text="button 1"
             title="Кнопка 1"
           >
             Подход
           </Button>
-          <Button onClick={() => handleClick('easy')} text="" title="Кнопка 2">
+          <Button
+            active={contentType === 'easy'}
+            onClick={() => handleClick('easy')}
+            text=""
+            title="Кнопка 2"
+          >
             Доступность
           </Button>
           <Button
+            active={contentType === 'program'}
             onClick={() => handleClick('program')}
             text="button 3"
             title=""
           >
             Концентрация
           </Button>
+
           {contentType ? (
             <p>{differences[contentType]}</p>
           ) : (
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import { act } from 'react'
 import './button.css'
 
-export default function Button({
-  text,
-  active = '',
-  children,
-  title,
-  onClick,
-}) {
+export default function Button({ text, active, children, title, onClick }) {
   return (
     <button
-      className={`button ${active}`}
+      className={active ? 'button active' : 'button'}
       onClick={onClick}
     >{`${children} ${text} ${title}`}</button>
   )

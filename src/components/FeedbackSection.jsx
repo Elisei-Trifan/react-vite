1c8df25d39c4728243730167d6a3884e1475b841
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-10 21:14:34 +0200

    стили для формы

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -2,6 +2,10 @@ export default function FeedbackSection() {
   return (
     <section>
       <h3>Обратная связь</h3>
+
+      <form>
+        <input type="text" className="control"></input>
+      </form>
     </section>
   )
 }

196801dc5324fde0edb081c8886fecd06ae84aa1
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-10 21:09:56 +0200

    добавлена библиотека styled-components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,30 @@
 // /* eslint-disable no-unused-vars */
 import { useState } from 'react'
+import { styled } from 'styled-components'
 import logo from '/vite.svg'
 
-import './header.css'
+// import './header.css'
 
-function Header() {
+const HeaderContainer = styled.header`
+  height: 50px;
+  display: flex;
+  padding: 0 2rem;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+  background: #fafafa;
+`
+
+export default function Header() {
   const [now, setNow] = useState(new Date())
 
   setInterval(() => setNow(new Date()), 1000)
 
   return (
-    <header>
+    <HeaderContainer>
       {/* <h3>Result University</h3> */}
       <img src={logo} alt="img"></img>
       <span>Время сейчас: {now.toLocaleTimeString()} </span>
-    </header>
+    </HeaderContainer>
   )
 }
-
-export default Header

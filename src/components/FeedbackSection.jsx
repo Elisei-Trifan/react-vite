4dd1ab8cd892969ebddf38873a0702d06dcee94f
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-11 11:22:57 +0200

    useRef

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,41 +1,90 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Button from './Button/Button'
 
+function StateVsRef() {
+  const inputt = useRef()
+  // const [input, setInput] = useState('')
+  const [show, setShow] = useState(false)
+
+  function handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      setShow(true)
+    }
+  }
+
+  // function handleChangeInput(e) {
+  //   setInput(e.target.value)
+  // }
+
+  return (
+    <div>
+      <h3>Input value: {show && inputt.current.value}</h3>
+      <input
+        ref={inputt}
+        type="text"
+        className="control"
+        // value={input}
+        onKeyDown={handleKeyDown}
+        // onChange={handleChangeInput}
+      ></input>
+    </div>
+  )
+}
+
 export default function FeedbackSection() {
-  const [name, setName] = useState('')
-  const [reason, setReason] = useState('help')
-  const [hasError, setHasError] = useState(false)
+  const [form, setForm] = useState({
+    name: '',
+    reason: 'help',
+    hasError: false,
+  })
+  // const [name, setName] = useState('')
+  // const [reason, setReason] = useState('help')
+  // const [hasError, setHasError] = useState(true)
 
   function handleNameChange(event) {
-    setName(event.target.value)
-    setHasError(event.target.value.trim().length === 0)
+    // setName(event.target.value)
+    // setHasError(event.target.value.trim().length === 0)
+    setForm((prev) => ({
+      ...prev,
+      name: event.target.value,
+      hasError: event.target.value.trim().length === 0,
+    }))
   }
 
   function handleReasonChange(e) {
-    setReason(e.target.value)
+    // setReason(e.target.value)
+    setForm((prev) => ({
+      ...prev,
+      reason: e.target.value,
+    }))
   }
 
-  function toggleError() {
-    setHasError(!hasError)
-  }
+  // function toggleError() {
+  // /* Неправльно, т.к. компонент не был перерисован, и предыдущее состояние не взялось*/
+  // setHasError(!hasError) //false
+  // setHasError(!hasError) // true
+  /* Правильно, передать нужно фукнцию, которая основывается на предыдущем состоянии*/
+  //   setHasError((prev) => !prev) // false
+  //   setHasError((prev) => !prev) // true
+  // }
 
   return (
     <section>
       <h3>Обратная связь</h3>
 
-      <Button onClick={toggleError}>Toggle Error</Button>
+      {/* <Button onClick={toggleError}>Toggle Error</Button> */}
 
-      <form>
+      <form style={{ marginBottom: '1rem' }}>
         <label htmlFor="name">Ваше имя</label>
         <input
           style={{
-            border: hasError ? '1px solid red ' : null,
+            border: form.hasError ? '1px solid red ' : null,
           }}
           type="text"
           id="name"
           className="control"
-          value={name}
+          value={form.name}
           onChange={handleNameChange}
         ></input>
 
@@ -43,7 +92,7 @@ export default function FeedbackSection() {
         <select
           id="reason"
           className="control"
-          value={reason}
+          value={form.reason}
           onChange={handleReasonChange}
         >
           <option value="error">Ошибка</option>
@@ -52,15 +101,16 @@ export default function FeedbackSection() {
         </select>
 
         <pre>
-          Name: {name}
+          Name: {form.name}
           <br />
-          Reason: {reason}
+          Reason: {form.reason}
         </pre>
 
-        <Button disabled={hasError} active={!hasError}>
+        <Button disabled={form.hasError} active={!form.hasError}>
           Отправить
         </Button>
       </form>
+      <StateVsRef />
     </section>
   )
 }
